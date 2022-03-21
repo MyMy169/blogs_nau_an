e71b71c9a9afae2938e078f1aa3499c0c38a5d70
My <53817900+MyMy169@users.noreply.github.com>
2022-03-21 17:41:00 +0700

    update2

--- reactjs_blog/src/header/Header.jsx
+++ reactjs_blog/src/header/Header.jsx
@@ -1,12 +1,11 @@
 import "./header.css"
-import banner from "./banner.jpg"
 export default function Header(){
     return(
         <div className="header">
             <div className="headerTitles">
                 <span className="headerTitleBlog">Blog</span>              
             </div>
-            <img className="headerImg" src={banner} alt="Banner"/>
+            <img className="headerImg" src="images/banner.jpg" alt="Banner"/>
         </div>
     );
 }
