88f632379afb9a0cfec44d6c21e4b1522046cd88
vothituyethong <63567624+vothituyethong@users.noreply.github.com>
2022-04-03 23:47:13 +0700

    ae2-3

---
+++ advanced_example/video-player/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import ReactPlayer from 'react-player'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReactPlayer url = 'https://www.youtube.com/watch?v=7sDY4m8KNLc' />
+      
+    </div>
+  );
+}
+
+export default App;
